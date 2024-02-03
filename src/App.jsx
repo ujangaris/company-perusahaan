3dd0158d21d2b8635e22eadbaf2c5f082140548c
Ujang Arisandi <ujangaja@gmail.com>
2024-02-03 16:08:40 +0700

    togle pada menu ketika didevice  mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,49 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Hamburger
+  const [isHamburgerActive, setHamburgerActive] = useState(false);
+  const toggleHamburger = () => {
+    setHamburgerActive(!isHamburgerActive);
+  };
+
   return (
     <>
-      <h1 className="text-red-500">Text Berwarna merah</h1>
+      {/* Navbar Section */}
+      <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+        <div className="container">
+          <div className="flex items-center justify-between relative">
+            <div className="px-4 flex items-center">
+              <a href="#home" className="font-bold text-lg  block py-6">
+                <img
+                  src="../src/assets/Logo 1.png"
+                  alt="Logo"
+                  className="size-7"
+                />
+              </a>
+              <h1 className="text-6 font-bold">
+                PT.BINA <span className="text-red-600"> DINAMITA</span> RAMA
+              </h1>
+            </div>
+            <div className="px-4 flex items-center">
+              <button
+                onClick={toggleHamburger}
+                id="hamburger"
+                name="hamburger"
+                type="button"
+                className={`block absolute right-4 ${
+                  isHamburgerActive ? "hamburger-active" : ""
+                }`}
+              >
+                <span className="hamburger-line transition duration-300 ease-in-out origin-left"></span>
+                <span className="hamburger-line transition duration-300 ease-in-out"></span>
+                <span className="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Akhir Navbar Section */}
     </>
   );
 }
