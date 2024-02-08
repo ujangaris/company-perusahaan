32da581c0c42471f2ff9ccb66c24508f8eff193b
Ujang Arisandi <ujangaja@gmail.com>
2024-02-08 18:28:54 +0700

    update slide image

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-const data = [
-  { img: "../../src/assets/jepang.png", name: "Jepang" },
-  { img: "../../src/assets/paris.png", name: "Paris" },
-  { img: "../../src/assets/indonesia.png", name: "Indonesia" },
-  { img: "../../src/assets/jepang.png", name: "Jepang" },
-  { img: "../../src/assets/paris.png", name: "Paris" },
-  { img: "../../src/assets/indonesia.png", name: "Indonesia" },
-  { img: "../../src/assets/jepang.png", name: "Jepang" },
-  { img: "../../src/assets/paris.png", name: "Paris" },
-  { img: "../../src/assets/indonesia.png", name: "Indonesia" },
-];
+import jepang from "../../src/assets/jepang.png";
+import paris from "../../src/assets/paris.png";
+import indonesia from "../../src/assets/indonesia.png";
+// const data = [
+//   { img: "../../src/assets/jepang.png", name: "Jepang" },
+//   { img: "../../src/assets/paris.png", name: "Paris" },
+//   { img: "../../src/assets/indonesia.png", name: "Indonesia" },
+//   { img: "../../src/assets/jepang.png", name: "Jepang" },
+//   { img: "../../src/assets/paris.png", name: "Paris" },
+//   { img: "../../src/assets/indonesia.png", name: "Indonesia" },
+//   { img: "../../src/assets/jepang.png", name: "Jepang" },
+//   { img: "../../src/assets/paris.png", name: "Paris" },
+//   { img: "../../src/assets/indonesia.png", name: "Indonesia" },
+// ];
 var settings = {
   dots: true,
   infinite: false,
@@ -74,25 +77,50 @@ export const Destination = () => {
           <div className="w-3/4 m-auto">
             <div className="mt-">
               <Slider {...settings}>
-                {data.map((d) => (
-                  <div
-                    key={d.name}
-                    className="w-full flex flex-wrap justify-center"
-                  >
-                    <div className="mb-12 ">
-                      <div className="group relative rounded-md overflow-hidde flex justify-center">
-                        <img
-                          src={d.img}
-                          alt={d.name}
-                          className=" w-[300px] h-[320px] xl:w-[300px] xl:h-[320px] border-4 group-hover:border-rose-500 border-white rounded-3xl shadow-2xl"
-                        />
-                        <button className="text-sm  md:text-base font-bold py-2 px-3 md:py-2 md:px-6 group-hover:bg-rose-500 bg-[#29BF08] rounded-full text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-24 md:-translate-y-1/2 xl:mt-28 lg:mt-20 md:mt-20 ">
-                          {d.name}
-                        </button>
-                      </div>
+                {/* {data.map((d) => ( */}
+                <div className="w-full flex flex-wrap justify-center">
+                  <div className="mb-12 ">
+                    <div className="group relative rounded-md overflow-hidde flex justify-center">
+                      <img
+                        src={jepang}
+                        alt={jepang}
+                        className=" w-[300px] h-[320px] xl:w-[300px] xl:h-[320px] border-4 group-hover:border-white border-[#29BF08] rounded-3xl shadow-2xl"
+                      />
+                      <button className="text-sm  md:text-base font-bold py-2 px-3 md:py-2 md:px-6 group-hover:bg-white bg-[#29BF08] rounded-full text-white group-hover:text-[#29BF08]  absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-24 md:-translate-y-1/2 xl:mt-28 lg:mt-20 md:mt-20 ">
+                        Jepang
+                      </button>
                     </div>
                   </div>
-                ))}
+                </div>
+                <div className="w-full flex flex-wrap justify-center">
+                  <div className="mb-12 ">
+                    <div className="group relative rounded-md overflow-hidde flex justify-center">
+                      <img
+                        src={paris}
+                        alt={paris}
+                        className=" w-[300px] h-[320px] xl:w-[300px] xl:h-[320px] border-4 group-hover:border-white border-[#F24E1E] rounded-3xl shadow-2xl"
+                      />
+                      <button className="text-sm  md:text-base font-bold py-2 px-3 md:py-2 md:px-6 group-hover:bg-white  bg-[#F24E1E] rounded-full text-white group-hover:text-[#F24E1E] absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-24 md:-translate-y-1/2 xl:mt-28 lg:mt-20 md:mt-20 ">
+                        Paris
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full flex flex-wrap justify-center">
+                  <div className="mb-12 ">
+                    <div className="group relative rounded-md overflow-hidde flex justify-center">
+                      <img
+                        src={indonesia}
+                        alt={indonesia}
+                        className=" w-[300px] h-[320px] xl:w-[300px] xl:h-[320px] border-4 group-hover:border-white border-[#EC5757] rounded-3xl shadow-2xl"
+                      />
+                      <button className="text-sm  md:text-base font-bold py-2 px-3 md:py-2 md:px-6 group-hover:bg-white bg-[#EC5757] rounded-full text-white group-hover:text-[#EC5757] absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-24 md:-translate-y-1/2 xl:mt-28 lg:mt-20 md:mt-20 ">
+                        Indonesia
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                {/* ))} */}
               </Slider>
             </div>
           </div>
