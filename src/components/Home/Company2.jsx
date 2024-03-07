558252e9f25004a2f59e4eff881f51e7e41bdbf3
Ujang Arisandi <ujangaja@gmail.com>
2024-03-07 20:50:34 +0700

    update company

--- src/components/Home/Company2.jsx
+++ src/components/Home/Company2.jsx
@@ -13,13 +13,14 @@ export const Company2 = () => {
           <div className="grid md:grid-cols-2 mx-7 lg:mx-20 xl:mx-32 gap-14 lg:gap-10">
             <div className="mt-[200px] md:mt-[190px] lg:mt-60 ">
               <h1 className="text-xl lg:text-4xl  font-semibold text-white mb-5 md:mb-2 ">
-                Companies That <br />
-                Use Bina Dinamita Rama
+                Perusahaan yang menggunakan jasa kami
               </h1>
               <p className="text-sm lg:text-base  xl:mt-10 font-semibold text-white">
-                selects graduates from accredited Tourist Colleges in Indonesia
-                who have worked in 4 - 5 stars Hotels and Fine Dinning
-                Restourants dealing with foreign guest
+                PT Bina Dinamita Rama bertekad untuk memenuhi kebutuhan sumber
+                daya manusia dibarengi kualitas yang terjamin. Perusahaan kami
+                mampu untuk memenuhi kebutuhan dari berbagai bidang dengan
+                berjalannya kerjasama yang berkelanjutan dengan berbagai lembaga
+                pelatihan yang tersebar di seluruh Indonesia.
               </p>
             </div>
             <div className="grid grid-cols-3  gap-2 lg:gap-0 mt-2 md:mt-[190px] lg:mt-60  px-3">
@@ -37,27 +38,27 @@ export const Company2 = () => {
           {/* Experience */}
           <div className="grid grid-cols-1 md:grid-cols-3 mt-12 md:mt-28 mx-7 md:mx-7 lg:mx-40  gap-y-10">
             <div className="years ">
-              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5">
-                20 Years +
+              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5 hover:font-bold">
+                20 Tahun +
               </h1>
-              <p className="text-xl text-white flex justify-center ">
-                Experience
+              <p className="text-xl text-white flex justify-center hover:font-bold">
+                Pengalaman
               </p>
             </div>
             <div className="users">
-              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5">
+              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5 hover:font-bold">
                 20K+
               </h1>
-              <p className="text-xl text-white flex justify-center ">
-                User Active
+              <p className="text-xl text-white flex justify-center hover:font-bold">
+                Pengguna Aktif
               </p>
             </div>
             <div className="customer">
-              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5">
+              <h1 className="text-5xl font-semibold text-[#29BF08] flex justify-center mb-5 hover:font-bold">
                 10K+
               </h1>
-              <p className="text-xl text-white flex justify-center ">
-                Happy Customers
+              <p className="text-xl text-white flex justify-center hover:font-bold ">
+                Pelanggan Puas
               </p>
             </div>
           </div>
